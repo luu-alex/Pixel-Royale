595ff6bcf4caf474b9b7a08e5cea9e70ffdedf26
luu-alex <luu.alex98@gmail.com>
2019-03-05 06:20:44 -0500

    Added new routes for login and added sqlite3

--- ftd/app.js
+++ ftd/app.js
@@ -1,6 +1,17 @@
 var express = require('express')
-var router  = express.Router();
-var app = express();
+const sqlite3 = require('sqlite3').verbose();
+
+
+var app = express(); //create a new express app
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+db.run('DROP TABLE IF EXISTS langs');
+db.run('CREATE TABLE IF NOT EXISTS langs(id INTEGER PRIMARY KEY AUTOINCREMENT,name text,password text)');
+
 
 const http = require('http');
 const hostname = '127.0.0.1';
@@ -8,14 +19,35 @@ const port = 3000;
 
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + './'));
 app.use(express.static('public'));
-
-router.get('/', function(req, res){
+app.get('/', function(req, res){
   res.render("index");
 })
-router.get('/login', function(req, res){
-  res.render('login');
+app.get('/login', function(req, res){
+  res.render("login");
+})
+app.post('/login', function(req, res){
+
+  db.run(`INSERT INTO langs(id,name,password) VALUES(?,?,?)`, [1,"alex","luu"], function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      // get the last insert id
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+    });
+  res.render("registration")
+})
+app.get('/database', function(req, res){
+  let sql = ("SELECT * FROM langs;");
+  db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row.name);
+  });
+});
+  res.send("run");
 })
 // const server = http.createServer((req, res) => {
 //   res.statusCode = 200;
