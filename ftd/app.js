7e4b07bd4af2a56712a4efc948672946d7db6cd2
Alex <alex.luu@mail.utoronto.ca>
2019-03-05 20:40:28 -0500

    added more routes

--- ftd/app.js
+++ ftd/app.js
@@ -1,8 +1,13 @@
 var express = require('express')
+var bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 
 
 var app = express(); //create a new express app
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 let db = new sqlite3.Database(':memory:', (err) => {
   if (err) {
     return console.error(err.message);
@@ -11,50 +16,58 @@ let db = new sqlite3.Database(':memory:', (err) => {
 });
 db.run('DROP TABLE IF EXISTS langs');
 db.run('CREATE TABLE IF NOT EXISTS langs(id INTEGER PRIMARY KEY AUTOINCREMENT,name text,password text)');
-
+var count = 0;
 
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.static('views'))
 app.get('/', function(req, res){
   res.render("index");
 })
+app.get('/registration', function(req, res){
+  res.render("registration")
+})
+app.post('/registration', function(req, res){
+  var sql = "SELECT name FROM langs where name="+"'"+req.body.name+"'"+";";
+  var a =0;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      if (row){
+        a = 1
+      }
+    })
+    if(a==0){
+      console.log("row not found")
+    }
+  });
+});
 app.get('/login', function(req, res){
   res.render("login");
 })
 app.post('/login', function(req, res){
-
-  db.run(`INSERT INTO langs(id,name,password) VALUES(?,?,?)`, [1,"alex","luu"], function(err) {
-      if (err) {
-        return console.log(err.message);
-      }
-      // get the last insert id
-      console.log(`A row has been inserted with rowid ${this.lastID}`);
-    });
-  res.render("registration")
+    res.send("hello")
 })
+
 app.get('/database', function(req, res){
+  var word = "";
   let sql = ("SELECT * FROM langs;");
   db.all(sql, [], (err, rows) => {
   if (err) {
     throw err;
   }
   rows.forEach((row) => {
-    console.log(row.name);
+    console.log(row);
   });
 });
-  res.send("run");
+  res.send("call");
 })
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
