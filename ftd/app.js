b133f8fd0d4f9a0057f57a53624a244ad42fd3c3
luu-alex <luu.alex98@gmail.com>
2019-03-05 23:04:10 -0500

    Added styles.css as well created an index page.

--- ftd/app.js
+++ ftd/app.js
@@ -1,36 +1,36 @@
-var express = require('express')
 var bodyParser = require('body-parser');
+var path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 
-
+var express = require('express')
 var app = express(); //create a new express app
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-let db = new sqlite3.Database(':memory:', (err) => {
+
+let db = new sqlite3.Database(':memory:', (err) => { //Attempt to connect to our database
   if (err) {
     return console.error(err.message);
   }
   console.log('Connected to the in-memory SQlite database.');
 });
-db.run('DROP TABLE IF EXISTS langs');
-db.run('CREATE TABLE IF NOT EXISTS langs(id INTEGER PRIMARY KEY AUTOINCREMENT,name text,password text)');
-var count = 0;
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+db.run('DROP TABLE IF EXISTS langs'); //Drop our current database when we run the server and create a new one
+db.run('CREATE TABLE IF NOT EXISTS langs(name text PRIMARY KEY,password text)');
 
-
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({extended: true}));  // to support URL-encoded bodies
 app.use(express.static('public'));
-app.use(express.static('views'))
+app.use(express.static('views'));
+
+//routes
 app.get('/', function(req, res){
   res.render("index");
-})
+});
+app.get('/game', function(req, res){
+  res.sendFile(__dirname +'/views/game.html');
+});
+
 app.get('/registration', function(req, res){
-  res.render("registration")
-})
+  res.sendFile(__dirname +'/views/registration.html');
+});
 app.post('/registration', function(req, res){
   var sql = "SELECT name FROM langs where name="+"'"+req.body.name+"'"+";";
   var a =0;
@@ -41,19 +41,27 @@ app.post('/registration', function(req, res){
     rows.forEach((row) => {
       if (row){
         a = 1
+        res.send("Error, account name already taken.");
       }
     })
     if(a==0){
-      console.log("row not found")
+      db.run(`INSERT INTO langs(name,password) VALUES(?,?)`, [req.body.name,req.body.psw], function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      // get the last insert id
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+      });
+      res.sendFile(__dirname +"/views/login.html")
     }
   });
 });
 app.get('/login', function(req, res){
-  res.render("login");
-})
+  res.sendFile(__dirname +"/views/login.html");
+});
 app.post('/login', function(req, res){
-    res.send("hello")
-})
+    res.send("hell")
+});
 
 app.get('/database', function(req, res){
   var word = "";
@@ -67,7 +75,15 @@ app.get('/database', function(req, res){
   });
 });
   res.send("call");
-})
+});
+
+app.get('/stats', function(req, res){
+  res.sendFile(__dirname +"/views/stats.html");
+});
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
