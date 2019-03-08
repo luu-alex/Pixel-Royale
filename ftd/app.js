e38659dd601205c2edb2bdb4b51494f87a1547f9
Alex <alex.luu@mail.utoronto.ca>
2019-03-07 22:07:25 -0500

    changing routing to create a single page application

--- ftd/app.js
+++ ftd/app.js
@@ -15,7 +15,7 @@ let db = new sqlite3.Database(':memory:', (err) => { //Attempt to connect to our
 db.run('DROP TABLE IF EXISTS langs'); //Drop our current database when we run the server and create a new one
 db.run('CREATE TABLE IF NOT EXISTS langs(name text PRIMARY KEY,password text,email varchar(255))');
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true}));  // to support URL-encoded bodies
 app.use(express.static('public'));
 app.use(express.static('views'));
@@ -32,6 +32,9 @@ app.get('/registration', function(req, res){
   res.sendFile(__dirname +'/views/registration.html');
 });
 app.post('/registration', function(req, res){
+  console.log(req.body.name)
+  console.log(req.body.email)
+  console.log(req.body.pass)
   var sql = "SELECT name FROM langs where name="+"'"+req.body.name+"'"+";";
   var a =0;
   db.all(sql, [], (err, rows) => {
@@ -41,26 +44,38 @@ app.post('/registration', function(req, res){
     rows.forEach((row) => {
       if (row){
         a = 1
-        res.send("Error, account name already taken.");
+        res.send("Error");
       }
     })
-    if(a==0){
-      db.run(`INSERT INTO langs(name,password,email) VALUES(?,?,?)`, [req.body.name,req.body.psw,req.body.email], function(err) {
-      if (err) {
-        return console.log(err.message);
-      }
-      // get the last insert id
-      console.log(`A row has been inserted with rowid ${this.lastID}`);
-      });
-      res.sendFile(__dirname +"/views/login.html")
+    db.run(`INSERT INTO langs(name,password,email) VALUES(?,?,?)`, [req.body.name,req.body.pass,req.body.email], function(err) {
+    if (err) {
+      return console.log(err.message);
     }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+    res.send("success")
+    });
   });
 });
 app.get('/login', function(req, res){
   res.sendFile(__dirname +"/views/login.html");
 });
 app.post('/login', function(req, res){
-    res.send("hell")
+  let sql = ("SELECT * FROM langs;");
+  var found = false;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      var obj = {"name": row.name, "email":row.email};
+      console.log(obj);
+      if(row.name==req.body.name && row.password == req.body.pass){
+        found = true;
+      }
+    });
+  });
+  res.send(found);
 });
 
 app.get('/database', function(req, res){
--- ftd/public/singlepage.js
+++ ftd/public/singlepage.js
@@ -1,35 +1,109 @@
-function loadDoc() {
-  console.log("hi");
-  var phase = "login";
-  var xhttp = new XMLHttpRequest();
-  if (phase == "login"){
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("screen").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "login", true);
-    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send(null);
+function database(){
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("screen").innerHTML = this.responseText;
+    }
   }
-  if (phase == "database"){
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("screen").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "database", true);
-    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send(null);
-  }
-  if (phase == "register"){
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("screen").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "register", true);
-    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send(null);
+  xhttp.open("GET", "database", true);
+  // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(null);
+}
+function register(){
+  $('#register').submit(function(e){
+  	$('#cont').load('registration.html', function(
+  		responseTXT, statusTXT, xhr){
+  			if(statusTXT == "success")
+  				console.log("good stuff");
+  		}
+  	)
+  })
+}
+function json(){
+  $.getJSON('users.json', function(data){
+    $.each(data, function(i, user){
+      $('ul#users'.append('<li>'))
+    })
+  })
+  $.ajax({
+    method:'GET',
+    url: "nothing"
+  })
+}
+function userRegister(){
+    console.log("hi")
+		var name = $('#regUser').val();
+		var pass = $('#regPsw').val();
+    var email = $('#regEmail').val();
+		var url = "/registration";
+		$.ajax({url:url,
+            data:{
+            name:name,
+            pass:pass,
+            email:email},
+            method:"POST",
+            success: function(result){
+              if (result=="success")
+                indexPage()
+              //show error registration
+    }});
+}
+function signIn(){
+		var name = $('#uname').val();
+		var pass = $('#psw').val();
+		var url = "/login"
+    console.log(name);
+    if (name && pass){
+      $.ajax({url: "/login",
+              method:"POST",
+              data:{name:name,pass:pass},
+              success: function(result){
+                console.log(result)
+    }});
   }
 }
+function registerPage(){
+  $("#registration").show();
+  $("#login").hide();
+}
+function indexPage(){
+  $("#registration").hide();
+  $("#login").show();
+}
+$(function(){
+	// Setup all events here and display the appropriate UI
+	// Setup an onclick event for the #guessButton
+	$("#registration").hide();
+	$("#login").show();
+  $("#loginBTN").on('click',function(e){
+    e.preventDefault();
+    signIn();
+  })
+  $("#userRegistration").on('submit', function(e){
+    userRegister();
+  })
+  $("#registerPage").on('click', function(){
+    registerPage();
+  })
+  $("#backBTN").on('click', function(){
+    indexPage();
+  })
+  $("#registerBTN").on('click', function(e){
+    e.preventDefault();
+    userRegister();
+  })
+  $("#postForm").on('submit', function(e){
+    e.preventDefault();
+  })
+});
+
+
+// $(function(){
+//   $(document).ready(function(){
+//     $('#screen').load('login.html', function(
+//       responseTXT, statusTXT, xhr){
+//         if(statusTXT == "success")
+//           console.log("good stuff");
+//       }
+//     )
+//   });
+// })
